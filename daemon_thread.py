685e55c3889ae64509af1782c0fcc51cb167aa3e
Bayardo <bjlslopez@gmail.com>
2022-06-17 15:59:19 -0600

    multithreading and multiprocessing

---
+++ daemon_thread.py
@@ -0,0 +1,19 @@
+import threading
+import time
+
+
+def timer():
+    print()
+    count = 0
+    while True:
+        time.sleep(1)
+        count += 1
+        print("logged in for: ", count, "seconds")
+
+
+x = threading.Thread(target=timer, daemon=True)
+x.start()
+
+print(x.__getattribute__('daemon'))
+
+answer = input("Do you wish to exit?")
