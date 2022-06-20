15a999409c15f920d9cd68858c7dedc64d7f3b30
Bayardo <bjlslopez@gmail.com>
2022-06-20 15:36:01 -0600

    file dialog

---
+++ file_dialog.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import filedialog
+
+
+def openFile():
+    filepath = filedialog.askopenfilename(initialdir="/home/bayardolopez/PycharmProjects/helloWorld",
+                                          title="Open file, okay?",
+                                          filetypes=(("text files", "*.txt"), ("all files", "*.*")))
+    file = open(filepath, 'r')
+    print(file.read())
+    file.close()
+
+
+window = Tk()
+button = Button(text="Open", command=openFile)
+button.pack()
+window.mainloop()
